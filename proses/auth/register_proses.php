02a78922609f7f3b2ae45dd83ce7c52764e15b6f
abibayan <abibayan114@gmail.com>
2024-11-08 18:27:17 +0700

    Menambahkan Backend Pada Halaman User

--- proses/auth/register_proses.php
+++ proses/auth/register_proses.php
@@ -4,27 +4,32 @@ if (isset($_POST['submit'])) {
     $username = $_POST['username'];
     $password = $_POST['password'];
     $role = $_POST['role'];
-    if ($role == 'admin') {
-        $query = "INSERT INTO user (username, password, role) VALUES ('$username', '$password',' '$role')";
+    
+        $query = "SELECT * FROM user WHERE username = '$username'";
         $result = mysqli_query($conn, $query);
 
-        if ($result) {
-            echo "<script>alert('Pendaftaran sebagai admin berhasil!');</script>";
-            header('location:../../login.php');
+        if (mysqli_num_rows($result)) {
+        
+            echo "<script>
+                alert('Username Telah Digunakan!');
+                window.location.href = '../../register.php';
+                </script>";
+            
         } else {
-            echo "Gagal registrasi sebagai admin";
-        }
-    } else {
-        $query = "INSERT INTO user (username, password, role) VALUES ('$username', '$password', '$role')";
-        $result = mysqli_query($conn, $query);
 
-        if ($result) {
-            echo "<script>alert('Pendaftaran sebagai user berhasil!');</script>";
-            header('location:../../login.php');
-        } else {
-            echo "Gagal registrasi sebagai user";
+            $query_insert = "INSERT INTO user (username, password, role) VALUES ('$username', '$password', '$role')";
+           
+
+            if(mysqli_query($conn,$query_insert)){
+                echo "<script>
+                alert('Pendaftaran sebagai user berhasil!');
+                window.location.href = '../../login.php';
+                </script>";
+            } else {
+                 echo "Gagal registrasi sebagai user";
+            }
         }
-    }
+
 }
 
 
